53ceb5e9667752d502228e2bf3ed3cdb03a8348f
Mark Dujali <mddujali@gmail.com>
2025-02-28 04:40:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Laravel Passport
+A full OAuth2 server implementation.
+
+## Requirements
+- Composer 2.7.x or higher
+- 8.3.x or higher
+- Node.js 20.x.x or higher
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        if ($this->app->isLocal()) {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
